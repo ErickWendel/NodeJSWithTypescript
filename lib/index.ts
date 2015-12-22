24beea24dbeea23592e800267cc4076a9aaa291f
erick.workspace <erick.workspace@hotmail.com>
2015-12-22 13:53:36 -0200

    first commit

---
+++ lib/index.ts
@@ -0,0 +1,18 @@
+    /// <reference path="../typings/tsd.d.ts" />
+    /// <reference path="../typings/tsd.d.ts" />
+
+    import express = require("express");
+    
+    var app = express();
+    var port = parseInt(process.env.PORT, 10) || 5000;
+    app.set("port", port);
+
+    app.get("/", (request:express.Request, response:express.Response) => {
+        response.send("Hello World!");
+    });
+
+
+    app.listen(port, () => {
+        console.log("Node app is running at localhost:" + port);
+       ;
+    });
