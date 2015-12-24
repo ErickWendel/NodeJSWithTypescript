849b31f2fc2c5016049525689f1c3fdc84725a1a
erick.workspace <erick.workspace@hotmail.com>
2015-12-24 03:05:41 -0200

    added get and setters

--- src/app/model/HeroModel.ts
+++ src/app/model/HeroModel.ts
@@ -2,6 +2,21 @@ import BaseModel = require('./base/BaseModel');
 class HeroModel extends BaseModel {
     public power: string;
     public amountPeopleSaved: number;
+    
+    get power () {
+        return this.power
+    }
+    set power (power: string) {
+        this.power = power;
+    }
+    
+    get amountPeopleSaved() {
+        return this.amountPeopleSaved;
+    }
+    set amountPeopleSaved(amountPeopleSaved: number) {
+        this.amountPeopleSaved = amountPeopleSaved;
+    }
+    
 }
 Object.seal(HeroModel);
 export =  HeroModel;
--- src/app/model/SpartanModel.ts
+++ src/app/model/SpartanModel.ts
@@ -3,6 +3,14 @@ import BaseModel = require('./base/BaseModel');
 class SpartanModel extends BaseModel {
     public folk: string;
     public amountPeopleKilled: number;
+    
+    get folk() : string { 
+        return this.folk;
+    }
+    
+    set folk (folk: string): void {
+        this.folk = folk;
+    }
 }
 
 Object.seal(SpartanModel);
