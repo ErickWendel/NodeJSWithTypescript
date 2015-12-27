135ca24209c696d3a38c5fa6b622589e6170fe37
erick.workspace <erick.workspace@hotmail.com>
2015-12-27 03:54:15 -0200

    fix bugs in hero

--- src/app/repository/HeroRepository.ts
+++ src/app/repository/HeroRepository.ts
@@ -1,20 +1,13 @@
 import HeroModel = require('./../model/HeroModel');
-
 import IHeroModel = require('./../model/interfaces/IHeroModel');
 import HeroSchema = require('./../data_access/schemas/HeroSchema');
-
 import RepositoryBase = require('./base/RepositoryBase');
 
 class HeroRepository  extends RepositoryBase<IHeroModel> {
     constructor () {
         super(HeroSchema);
-    }
-    
+    }    
 } 
-
-new HeroRepository().create(<IHeroModel> { name: "test3", amountPeopleSaved: 2, power: "test"},
-     (e,r ) =>
-{ console.log('e', e); console.log('r', r)});
 
 Object.seal(HeroRepository);
 export = HeroRepository;
