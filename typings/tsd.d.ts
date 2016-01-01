995195bfe311b5b6735f28ca3807ef1477b67cb3
erick.workspace <erick.workspace@hotmail.com>
2016-01-01 19:58:14 -0200

    removed bluebird

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -4,6 +4,4 @@
 /// <reference path="mime/mime.d.ts" />
 /// <reference path="mongoose/mongoose.d.ts" />
 /// <reference path="body-parser/body-parser.d.ts" />
-/// <reference path="bluebird/bluebird.d.ts" />
 /// <reference path="method-override/method-override.d.ts" />
-undefined
